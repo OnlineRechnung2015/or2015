3cc6096bd5dccea76c37e77789338b3c8492e1fd
Jörg <OnlineRechnung2015@gmail.com>
2015-01-05 23:13:08 +0100

    New Project -> "onlineRechnung2015" aka "OR2k15"

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf8" />;
+    </head>
+    <body>
+        
+    <?php
+        echo "<h1>Hallo Welt!!! </h1>";
+
+        echo "<h3>Projekt OnlineRechnung2015</h3>";
+        echo "<p>Wilkommen bei meinem Abschlussprojekt OR2K15 <br />";
+        echo "hier entsteht ein web-basiertes Programm zur Rechnungsverwaltung </p>";
+        echo "Es wird über nachfolgende Funktionalität verfügen:";
+        echo "<ul><li>Kundenverwaltung</li><li>Warenwirtschaft</li><li>Rechnungsverwaltung</li></ul>";
+        echo "<hr /><p>Datum: ";
+        echo date("d.m.Y");
+        echo "</p>";
+    ?>
+    </body>
+</html>
